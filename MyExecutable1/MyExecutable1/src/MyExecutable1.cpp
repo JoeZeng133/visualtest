4695430de9974fd971b90edf428e8cc216a34dcf
JoeZeng133 <35885015+JoeZeng133@users.noreply.github.com>
2018-01-29 00:31:08 -0500

    stage

---
+++ MyExecutable1/MyExecutable1/src/MyExecutable1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+	std::cout << "That is different";
+
+
+	getchar();
+}
